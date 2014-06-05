ec0532ba168c649cd445250798e2795cd6be5be8
Naveen Kumashi <naveen.kumashi@hp.com>
2014-06-05 17:05:16 +0530

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by SauravKabra on 6/5/14.
+//  Copyright (c) 2014 HewlettPackard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by SauravKabra on 6/5/14.
+//  Copyright (c) 2014 HewlettPackard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
